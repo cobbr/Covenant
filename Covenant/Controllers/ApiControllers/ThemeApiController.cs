df4e10f9d223aa848af31afdce868c7c31f6ff1f
mrjamiebowman <mrjamiebowman@gmail.com>
2019-12-02 00:54:05 -0500

    ThemeApi controller to return custom style.css, layout to ref this endpoint, html/js for dark mode switch, also removed bg-light and pulling that color from the style.css (Standard or Dark)

---
+++ Covenant/Controllers/ApiControllers/ThemeApiController.cs
@@ -0,0 +1,51 @@
+﻿// Author: Ryan Cobb (@cobbr_io)
+// Project: Covenant (https://github.com/cobbr/Covenant)
+// License: GNU GPLv3
+
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+
+using Covenant.Core;
+using Covenant.Models;
+using Covenant.Models.Covenant;
+using Covenant.Models.Listeners;
+using System;
+
+namespace Covenant.Controllers.ApiControllers
+{
+    [Authorize(Policy = "RequireJwtBearer")]
+    [ApiController]
+    [Route("api/themes")]
+    public class ThemeApiController : Controller
+    {
+        private readonly CovenantContext _context;
+        private readonly UserManager<CovenantUser> _userManager;
+
+        public ThemeApiController(CovenantContext context, UserManager<CovenantUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("style.css")]
+        public async Task<IActionResult> GetThemeCss()
+        {
+            try
+            {
+                string standardThemeCss = await _context.GetThemeCss(ThemeType.Standard);
+                string darkThemeCss = await _context.GetThemeCss(ThemeType.Dark);
+                string css = standardThemeCss + "\n\n\n" + darkThemeCss;
+                return Content(css, "text/css");
+            }
+            catch (Exception ex)
+            {
+                return Content(string.Empty, "text/css");
+            }            
+        }
+    }
+}
