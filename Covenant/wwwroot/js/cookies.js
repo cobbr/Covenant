5ece56a0cdc0ca488684f275adbc56521f6d0781
mrjamiebowman <mrjamiebowman@protonmail.com>
2019-12-02 19:11:28 -0500

    bug fix for not displaying displaying standard mode on login screen... using javascript to get/set dark mode cookie so it persists between pages.

---
+++ Covenant/wwwroot/js/cookies.js
@@ -0,0 +1,12 @@
+﻿function getCookie(name) {
+    var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+    return v ? v[2] : null;
+}
+
+function setCookie(name, value, days) {
+    var d = new Date;
+    d.setTime(d.getTime() + 24 * 60 * 60 * 1000 * days);
+    document.cookie = name + "=" + value + ";path=/;expires=" + d.toGMTString();
+}
+
+function deleteCookie(name) { setCookie(name, '', -1); }
