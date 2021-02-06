e911f4a75f4bc0cfb18b19551c30750af718e146
cobbr <ryancobb65@gmail.com>
2021-02-05 20:45:22 -0600

    Added OutputKind to LauncherForm

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Added ability to specify custom admin port number
 - Added Covenant trace logging
 - Added ServiceBinary launcher, added PSExecCommand Task
+- Added OutputKind to LauncherForm
 
 ### Changed
 - Disallow Administrators from changing other user's passwords
--- Covenant/Models/Launchers/BinaryLauncher.cs
+++ Covenant/Models/Launchers/BinaryLauncher.cs
@@ -20,7 +20,7 @@ namespace Covenant.Models.Launchers
             this.Type = LauncherType.Binary;
             this.Description = "Uses a generated .NET Framework binary to launch a Grunt.";
             this.Name = "Binary";
-            this.OutputKind = OutputKind.WindowsApplication;
+            this.OutputKind = OutputKind.ConsoleApplication;
             this.CompressStager = false;
         }
 
